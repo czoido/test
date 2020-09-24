d7300aa81874a4c85e616e87421a2631ea67e285
czoido <mrgalleta@gmail.com>
2020-09-24 11:03:25 +0200

    wip

--- install_docker.sh
+++ install_docker.sh
@@ -27,6 +27,8 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-sudo docker run hello-world
+sudo gpasswd -a conan docker && newgrp docker && sudo chmod 666 /var/run/docker.sock
+
+docker run hello-world
 
 docker-compose --version
